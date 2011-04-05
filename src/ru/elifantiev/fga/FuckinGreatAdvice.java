e6773953a3f20a79503188397338bd3db14f6883
zoxa <azakhidov@gmail.com>
2011-04-04 21:19:21 -0400

    remove unused function

--- src/ru/elifantiev/fga/FuckinGreatAdvice.java
+++ src/ru/elifantiev/fga/FuckinGreatAdvice.java
@@ -1,18 +1,17 @@
 package ru.elifantiev.fga;
 
 
-import android.text.Html;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.text.Html;
 
 public class FuckinGreatAdvice {
 
@@ -35,49 +34,21 @@ public class FuckinGreatAdvice {
             Pattern p = Pattern.compile("id=\"advice\">([^<]+)</");
             Matcher m = p.matcher(page);
             while (m.find()) { // Find each match in turn; String can't do this.
-                retval =  m.group(1).replace("&nbsp;", " ");
+                // current advice
+            	retval =  m.group(1).replace("&nbsp;", " ");
+                
             }
 
             p = Pattern.compile("\"another\"><a href=\"([^\"]+)\"");
             m = p.matcher(page);
             while (m.find()) { // Find each match in turn; String can't do this.
-                url =  m.group(1);
+                // next advice
+            	url =  m.group(1);
             }
         }
         return Html.fromHtml(retval).toString();
     }
-
-    private Document loadAdvicePage() {
-
-        String input;
-
-        try {
-            input = loadPage();
-        } catch (IOException e) {
-            return null;
-        }
-
-        DocumentBuilder docB = null;
-        try {
-            docB = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            return null;
-        }
-
-        Document doc = null;
-        if (docB != null) {
-            try {
-                doc = docB.parse(new ByteArrayInputStream(input.getBytes()));
-            } catch (SAXException e) {
-                return null;
-            } catch (IOException e) {
-                // ignore
-            }
-        }
-
-        return doc;
-    }
-
+  
     private String loadPage() throws IOException {
         StringBuilder response = new StringBuilder();
         String line;
