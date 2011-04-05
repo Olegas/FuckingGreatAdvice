937ab0a7f197f419187b511ed19dcc8d59d4f28f
zoxa <azakhidov@gmail.com>
2011-04-04 22:04:00 -0400

    widget: - update receiver - widget provider - simple widget pull - widget update (each 2 hours)
    todo:
    - widget config
    - widget update (configurable)
    - widget next advice
    - widget tempalte

---
+++ src/ru/elifantiev/fga/widget/FuckingWidget.java
@@ -0,0 +1,34 @@
+/**
+ * Widget Related packages:
+ * - AppWidgetProvider
+ * - WidgetUpdater
+ * - WidgetConfig
+ * - WidgetUpdateService
+ */
+package ru.elifantiev.fga.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * @author zoxa
+ */
+public class FuckingWidget extends AppWidgetProvider
+{
+
+    public void onUpdate( final Context context, final AppWidgetManager appWidgetManager,
+            final int[] appWidgetIds )
+    {
+        final int N = appWidgetIds.length; 
+
+        for ( int i = 0; i < N; i++ )
+        {
+            WidgetUpdater wUpdater = new WidgetUpdater( context, appWidgetManager,
+                    appWidgetIds[i] );
+            new Thread( wUpdater ).start();
+        } 
+        
+        super.onUpdate( context, appWidgetManager, appWidgetIds );
+    }
+}
